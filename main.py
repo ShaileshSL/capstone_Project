a2af91073d02f9b7316928630554a9f92a2b297f
ShaileshSL <155991917+ShaileshSL@users.noreply.github.com>
2026-04-21 10:02:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from lib.logger import Log4j
 if __name__ == '__main__':
 
     if len(sys.argv) < 3:
-        print("Usage: sbdl {local, qa, prod} {load_date} : Arguments are missing")
+        print("Usage: sbdl {local, qa, prod} {load_date} : Arguments are missing.")
         sys.exit(-1)
 
     job_run_env = sys.argv[1].upper()
